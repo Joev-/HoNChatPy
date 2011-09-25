0a3f4c96ad06712783af55326d49518eb5ac7ee8
Joseph Vaughan <joe.vaughan92@gmail.com>
2011-09-25 18:49:45 +0100

    Some structural re-thinks, a bit cleaner.

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,3 +1,1 @@
-import construct
-import phpserialize
 import honcore
--- main.py
+++ main.py
@@ -1,73 +1,75 @@
 #!/usr/bin/env python
-import sys, threading, signal, re, time, hashlib, getpass
-
+import sys, threading, signal, re, time, getpass
+from hashlib import md5
 try:
-	from lib.honcore import client
-	from lib.honcore.exceptions import *
+    from lib.honcore.client import HoNClient
+    from lib.honcore.exceptions import *
 except ImportError:
-	print "HoNCore could not be found in the lib folder, please ensure it is available."
+    print "HoNCore could not be found in the lib folder, please ensure it is available."
 
 from core import *
 
-def md5hash(password):
-	""" Hashes a password to MD5 """
-	return hashlib.md5(password).hexdigest()
+class HoNChatPy(HoNClient):
+    def __init__(self):
+        super(HoNChatPy, self).__init__()
+        self.logged_in = False
 
-chat_client = client.HoNClient()
+    @property
+    def is_logged_in(self):
+        return self.logged_in
+
+    def login(self, username, password):
+        log.info("Logging in..")
+        try:
+            self._login(username, password)
+        except MasterServerError, e:
+            log.error(e)
+
+        try:
+            self._chat_connect()
+        except ChatServerError, e:
+            log.error(e)
+
+        self.logged_in = True
+
+    def logout(self):
+        log.info("Logging out...")
+        try:
+            self._chat_disconnect()
+        except ChatServerError, e:
+            log.error(e)
+
+        try:
+            self._logout()
+        except MasterServerError, e:
+            log.error(e)
+
+        self.logged_in = False
 
 def main():
+    while 1:
+        while honchatpy.is_logged_in is False:
+            usr = raw_input("Username: ")
+            passw = getpass.getpass()
+            passw = md5(passw).hexdigest()
 
-	while not chat_client.is_logged_in():
-		usr = raw_input("Username: ")
-		passw = getpass.getpass()
-
-		log.info("Logging in...")
-		try:
-			chat_client.login(usr, md5hash(passw))
-		except MasterServerError, e:
-			log.error(e)
-	
-	if chat_client.is_logged_in():
-		log.info("Waiting for chat server to verify authentication.")
-		try:
-			chat_client.chat_connect()
-		except ChatServerError, e:
-			log.error(e)
-	
-	while chat_client.is_logged_in() and chat_client.is_connected():
-		time.sleep(1)
-		
-			
-def disconnect_logout():
-	if chat_client.is_connected():
-		log.info("Disconnecting from chat server")
-
-	try:
-		# TODO: Handle response from chat_disconnect
-		# Not much can fail because the socket gets killed anyway
-		# it's up to the chat server to clean up broken connections. :)
-		chat_client.chat_disconnect()
-	except ChatServerError, e:
-		pass
-	
-	if chat_client.is_logged_in():
-		log.info("Logging out")
-		
-	try:
-		# TODO: Handle requester response for logging out
-		chat_client.logout()
-	except MasterServerError, e:
-		if e.code == 106:
-			log.error('Logout was forced, master server did receive the logout request.')
-
-def sigint_handler(signum,	frame):
-	"""Handles SIGINT signal (<C-c>). Quits program."""
-	disconnect_logout()
-	log.info("Quitting...")
-	sys.exit(0)
+            honchatpy.login(usr, passw)
+             
+        while honchatpy.is_logged_in:
+            time.sleep(1)
+        
+def sigint_handler(signum,  frame):
+    """Handles SIGINT signal (<C-c>). Quits program."""
+    if honchatpy.is_logged_in:
+        honchatpy.logout()
+    else:
+        log.info("Quitting...")
+        sys.exit(0)
 
 if __name__ == "__main__":
-	signal.signal(signal.SIGINT, sigint_handler)
-	log.add_logger(sys.stdout, 'DEBUG', False)
-	log.add_logger('honchat_log', 'DEBUG', True)
-	main()
+    signal.signal(signal.SIGINT, sigint_handler)
+    log.add_logger(sys.stdout, 'DEBUG', False)
+    log.add_logger('honchat_log', 'DEBUG', True)
+    honchatpy = HoNChatPy()
+    main()
+
